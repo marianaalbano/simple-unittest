8eb91d816a4b2cfab2e3bdd827a72dcec4806738
Mariana Albano <mariana.albano@4linux.com.br>
2017-11-27 14:28:36 -0200

    unittest ok

---
+++ test.py
@@ -0,0 +1,27 @@
+from run import app
+import unittest
+
+
+class Test(unittest.TestCase):
+    
+    def setUp(self):
+        # cria uma instância do unittest, precisa do nome "setUp"
+        self.app = app.test_client()
+
+    def test_url(self):
+        # envia uma requisicao GET para a URL
+        result = self.app.get('/') 
+
+        # compara o status da requisicao (precisa ser igual a 200)
+        self.assertEqual(result.status_code, 200) 
+
+    def test_content(self):
+        # envia uma requisicao GET para a URL
+        result = self.app.get('/') 
+
+        # verifica o retorno do conteudo da pagina
+        self.assertEqual(result.data.decode(), "Hello world")
+
+
+if __name__ == "__main__":
+    unittest.main()
