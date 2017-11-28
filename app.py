e3c8e1915c735968a307a2c8da9f15f3ae61697f
Mariana Albano <mariana.albano@4linux.com.br>
2017-11-28 14:51:44 -0200

    teste 100

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "vini"
+    return "gaby"
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -8,20 +8,20 @@ class Test(unittest.TestCase):
         # cria uma instância do unittest, precisa do nome "setUp"
         self.app = app.test_client()
 
-    def test_url(self):
+    def test_requisicao(self):
         # envia uma requisicao GET para a URL
-        result = self.app.get('/') 
+        result = self.app.get('/')
 
         # compara o status da requisicao (precisa ser igual a 200)
         self.assertEqual(result.status_code, 200) 
 
-    def test_content(self):
+    def test_conteudo(self):
         # envia uma requisicao GET para a URL
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(result.data.decode(), "alissao")
+        self.assertEqual(result.data.decode(), "gaby")
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
