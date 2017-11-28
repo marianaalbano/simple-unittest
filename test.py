4199a82ba7b2a9c75abe1a092f7aa69e8afec490
Mariana Albano <mariana.albano@4linux.com.br>
2017-11-28 08:56:22 -0200

    teste heroku

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(result.data.decode(), "TESTE")
+        self.assertEqual(result.data.decode(), "mari")
 
 
 if __name__ == "__main__":
